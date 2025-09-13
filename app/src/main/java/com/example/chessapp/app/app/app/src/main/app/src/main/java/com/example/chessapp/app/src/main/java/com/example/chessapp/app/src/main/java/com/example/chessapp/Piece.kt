5b987c12d6680cee5d9b49f93c225561572296cd
tuylerdurden2333-oss <tuylerdurden2333@gmail.com>
2025-09-12 18:55:49 -0700

    Create Piece.kt

---
+++ app/src/main/java/com/example/chessapp/app/app/app/src/main/app/src/main/java/com/example/chessapp/app/src/main/java/com/example/chessapp/app/src/main/java/com/example/chessapp/Piece.kt
@@ -0,0 +1,9 @@
+package com.example.chessapp
+
+import com.github.bhlangonijr.chesslib.Piece
+import com.github.bhlangonijr.chesslib.Square
+
+data class ChessPiece(
+    val piece: Piece,
+    val square: Square
+)
